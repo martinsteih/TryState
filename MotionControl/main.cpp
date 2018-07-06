4bc332730476467aab9eee543946e2dc48138cc5
Martin Steih <martinsteih@gmx.de>
2018-07-06 14:12:20 +0200

    passing arguments to a states constructor works

--- MotionControl/main.cpp
+++ MotionControl/main.cpp
@@ -12,19 +12,32 @@ namespace sc = boost::statechart;
 namespace mpl = boost::mpl;
 
 //*********************Event definitions*************************
-struct event_pos_abs : sc::event<event_pos_abs> {};
+struct event_pos_abs : sc::event<event_pos_abs> {
+  explicit event_pos_abs(double position) : _position{position} {}
+  double get_position() const { return _position; }
+
+ private:
+  double _position;
+};
+
 struct event_jog_pos : sc::event<event_jog_pos> {};
+
 struct event_homing : sc::event<event_homing> {};
+
 struct event_reset : sc::event<event_reset> {};
+
 struct event_jog_neg : sc::event<event_jog_neg> {};
+
 struct event_move_velocity : sc::event<event_move_velocity> {};
-struct event_pos_rel : sc::event<event_pos_rel> {};
+
+struct event_pos_rel : sc::event<event_jog_neg> {};
+
+struct standstill;
 
 //*********************State Machine*************************
-struct standstill;
 class S120 : public sc::state_machine<S120, standstill> {
  public:
-  S120() { this->initiate(); }
+  S120() {}
   ~S120() {}
 
   auto homing() { this->process_event(event_homing()); }
@@ -32,77 +45,41 @@ class S120 : public sc::state_machine<S120, standstill> {
   auto move_jog_pos() { return this->process_event(event_jog_pos()); }
   auto move_jog_neg() { return this->process_event(event_jog_neg()); }
   auto move_velocity() { return this->process_event(event_move_velocity()); }
-  auto pos_absolute() { return this->process_event(event_pos_abs()); }
+  auto pos_absolute(double position) {
+    return this->process_event(event_pos_abs(position));
+  }
   auto pos_relative() { return this->process_event(event_pos_rel()); }
 };
 
 //*********************State definitions*************************
-
-struct idle;
-struct standstill : sc::simple_state<standstill, S120, idle> {
+struct operational;
+struct standstill : sc::simple_state<standstill, S120> {
   standstill() { cout << "standstill::ctor\n"; }
   ~standstill() { cout << "standstill::dtor\n"; }
+  typedef sc::transition<event_pos_abs, operational> reactions;
 };
 
-struct homing;
-struct move_jog;
-struct pos_abs;
-struct pos_rel;
-struct move_velo;
-
-struct operational : sc::simple_state<operational, S120, move_jog> {
-  operational() { cout << "operational::ctor\n"; }
-  ~operational() { cout << "operational::dtor\n"; }
-};
-
-struct idle : sc::simple_state<idle, standstill> {
-  idle() { cout << "idle::ctor\n"; }
-  ~idle() { cout << "idle::dtor\n"; }
-  typedef mpl::list<sc::transition<event_homing, homing>,
-		    sc::transition<event_jog_pos, move_jog>,
-		    sc::transition<event_jog_neg, move_jog>,
-		    sc::transition<event_pos_abs, pos_abs>,
-		    sc::transition<event_pos_rel, pos_rel>,
-		    sc::transition<event_move_velocity, move_velo>>
-      reactions;
-};
-
-struct reset_error : sc::simple_state<reset_error, standstill> {
-  reset_error() { cout << "reset_error::ctor\n"; }
-  ~reset_error() { cout << "reset_error::dtor\n"; }
-};
-
-struct homing : sc::simple_state<homing, operational> {
-  homing() { cout << "homing::ctor\n"; }
-  ~homing() { cout << "homing::dtor\n"; }
-};
-
-struct move_jog : sc::simple_state<move_jog, operational> {
-  move_jog() {
-    cout << "move_jog::ctor "
-	 << "\n";
+struct operational : sc::state<operational, S120> {
+  operational(my_context context) : my_base(context){
+    if (auto triggering(dynamic_cast<const event_pos_abs*>(triggering_event()));
+	triggering) {
+      cout << "operational::ctor\n";
+      cout << "moving to pos " << triggering->get_position() << endl;
+    }
   }
-  ~move_jog() { cout << "move_jog::dtor\n"; }
-};
-
-struct pos_abs : sc::simple_state<pos_abs, operational> {
-  pos_abs() { cout << "pos_abs::ctor\n"; }
-  ~pos_abs() { cout << "pos_abs::dtor\n"; }
-};
-
-struct pos_rel : sc::simple_state<pos_rel, operational> {
-  pos_rel() { cout << "pos_rel::ctor\n"; }
-  ~pos_rel() { cout << "pos_rel::dtor\n"; }
-};
-
-struct move_velo : sc::simple_state<move_velo, operational> {
-  move_velo() { cout << "move_velo::ctor\n"; }
-  ~move_velo() { cout << "move_velo::dtor\n"; }
+  ~operational() { cout << "operational::dtor\n"; }
+  typedef mpl::list<sc::custom_reaction<event_pos_abs>,
+		    sc::transition<event_jog_pos, standstill>>
+      reactions;
+  sc::result react(const event_pos_abs &) { cout << "operational::react()\n"; }
 };
 
 //*********************Event definitions*************************
 int main(int argc, char *argv[]) {
   S120 my_drive;
-  my_drive.pos_absolute();
+  my_drive.initiate();
+  my_drive.move_jog_pos();
+  my_drive.pos_absolute(100.0);
+  my_drive.pos_absolute(150.5);
   return 0;
 }
